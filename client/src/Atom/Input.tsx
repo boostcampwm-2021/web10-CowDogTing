a5d39be7fbc11cee17496e3b281222b09682888a
jin-Pro <dnjun2@ajou.ac.kr>
2021-11-01 19:57:53 +0900

    Feat : Input UI close #19

---
+++ client/src/Atom/Input.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export const Input = styled.input`
+  width: 300px;
+  height: 50px;
+  border: 2px solid #ffcfcf;
+  padding: 0 20px;
+  cursor: pointer;
+
+  &:focus {
+    outline: none;
+  }
+`;
