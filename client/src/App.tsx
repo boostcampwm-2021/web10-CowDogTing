3f027e6901531ce0bbec56a89867ebacada12908
taehong0-0 <xoghd5885@ajou.ac.kr>
2021-11-01 17:59:01 +0900

    Feat: Header 메뉴버튼 생성

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { Global } from "@emotion/react";
 import MainPage from "./Page/MainPage";
+import reset from "./util/reset";
+import LogInPage from "./Page/LogInPage";
+import Menu from "./Atom/Menu";
 
 function App() {
   return (
     <>
       <MainPage />
+      <Global styles={reset} />
+      <LogInPage />
+      <Menu />
     </>
   );
 }
