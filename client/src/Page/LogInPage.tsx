5104b1f24f4ceda798784cb31d042fa20aec4cd4
jin-Pro <dnjun2@ajou.ac.kr>
2021-11-01 18:36:56 +0900

    Fix : Button Type

--- client/src/Page/LogInPage.tsx
+++ client/src/Page/LogInPage.tsx
@@ -8,7 +8,7 @@ export default function LogInPage() {
       <Button type="Small">나가기</Button>
       <Button type="Medium">초대하기</Button>
       <Button type="Large">채팅 신청하기</Button>
-      <Button type="Long">4</Button>
+      <Button type="Long">5</Button>
       <Button type="DropDown">드랍다운</Button>
       <Test />
     </>
