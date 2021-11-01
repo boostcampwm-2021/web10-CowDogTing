d7f2466da712a7b3bfbfd53a2abc6792243c9610
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2021-11-01 19:31:27 +0900

    fix : 메인 페이지 헤더 로고 레이아웃 css 수정 #5

--- client/src/Molecules/MainHeaderCenter.tsx
+++ client/src/Molecules/MainHeaderCenter.tsx
@@ -1,11 +1,21 @@
+/** @jsxImportSource @emotion/react */
 import React from "react";
+import { css } from "@emotion/react";
 import MainHeaderLogo from "../Atom/MainHeaderLogo";
+
+const mainHeaderCenterStyle = css`
+  position: relative;
+  display: flex;
+  width: 100vw;
+  justify-content: center;
+  top: 30%;
+`;
 
 function MainHeaderCenter() {
   return (
-    <>
+    <div css={mainHeaderCenterStyle}>
       <MainHeaderLogo />
-    </>
+    </div>
   );
 }
 
