798fc339c5dd696468c6bd29f2ce5b37e7e2502c
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2021-11-01 19:27:22 +0900

    feat : MainPageBody left,right 프레임 추가 #6

--- client/src/Template/MainPageBody.tsx
+++ client/src/Template/MainPageBody.tsx
@@ -1,6 +1,8 @@
 /** @jsxImportSource @emotion/react */
 import React from "react";
 import { css } from "@emotion/react";
+import MainBodyLeft from "../Organism/MainBodyLeft";
+import MainBodyRight from "../Organism/MainBodyRight";
 
 const mainPageBodyStyle = css`
   width: 100vw;
@@ -11,7 +13,12 @@ const mainPageBodyStyle = css`
 `;
 
 function MainPageBody() {
-  return <div css={mainPageBodyStyle} />;
+  return (
+    <div css={mainPageBodyStyle}>
+      <MainBodyLeft />
+      <MainBodyRight />
+    </div>
+  );
 }
 
 export default MainPageBody;
