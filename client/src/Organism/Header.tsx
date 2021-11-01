676f5a65f23d44f99cf15b9a77b19a9130e8573c
taehong0-0 <xoghd5885@ajou.ac.kr>
2021-11-02 01:27:20 +0900

    Feat: Header 드롭다운 토글 이벤트 추가

--- client/src/Organism/Header.tsx
+++ client/src/Organism/Header.tsx
@@ -1,15 +1,20 @@
 import { css } from "@emotion/react";
+import { useState } from "react";
 import Menu from "../Atom/Menu";
 import DropDown from "../Molecules/DropDown";
 
 const HeaderStyle = css``;
 
 export default function Header() {
+  const [MenuOpen, setMenu] = useState(false);
+  const ToggleMenu = () => {
+    setMenu((isOpen) => !isOpen);
+  };
   return (
     <div css={HeaderStyle}>
       <div>
-        <Menu />
-        <DropDown type="Menu" />
+        <Menu onClick={() => ToggleMenu()} />
+        <DropDown type="Menu" className={MenuOpen ? "show" : "hide"} />
       </div>
     </div>
   );
