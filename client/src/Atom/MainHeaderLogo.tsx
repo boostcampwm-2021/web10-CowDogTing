74d975b07959a90a19b8d1bcfb8d955776156207
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2021-11-01 19:31:27 +0900

    fix : 메인 페이지 헤더 로고 레이아웃 css 수정 #5

--- client/src/Atom/MainHeaderLogo.tsx
+++ client/src/Atom/MainHeaderLogo.tsx
@@ -1,16 +1,8 @@
-/** @jsxImportSource @emotion/react */
 import React from "react";
-import { css } from "@emotion/react";
 import logo from "../assets/Logo.svg";
 
-const mainHeaderLogoStyle = css`
-  position: relative;
-  left: 40%;
-  top: 70%;
-`;
-
 function MainHeaderLogo() {
-  return <img alt="mainLogo" src={logo} css={mainHeaderLogoStyle} />;
+  return <img alt="mainLogo" src={logo} />;
 }
 
 export default MainHeaderLogo;
