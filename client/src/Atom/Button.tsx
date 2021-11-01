fd95c2ceb1e6ec5c950a02b9a3acfc7b9c57d357
jin-Pro <dnjun2@ajou.ac.kr>
2021-11-01 19:04:17 +0900

    Fix : Button Type State & Color

--- client/src/Atom/Button.tsx
+++ client/src/Atom/Button.tsx
@@ -1,6 +1,12 @@
+/* eslint-disable comma-dangle */
 import styled from "@emotion/styled";
 import { css } from "@emotion/react";
 import { ButtonType } from "../util/type";
+
+const StateButtonStyle = css`
+  width: 100px;
+  height: 50px;
+`;
 
 /* 나가기 버튼 */
 const SmallButtonStyle = css`
@@ -26,26 +32,34 @@ const LongButtonStyle = css`
 const SmallDropDownStyle = css`
   width: 200px;
   height: 65px;
+  border-radius: 0;
 `;
 
 const LargeDropDownStyle = css`
   width: 250px;
   height: 80px;
+  border-radius: 0;
 `;
 
 const ButtonStyle = (props: ButtonType) => css`
+  border: 2px solid ${props.color};
+  ${props.type === "State" && StateButtonStyle}
   ${props.type === "Small" && SmallButtonStyle}
   ${props.type === "Medium" && MediumButtonStyle}
   ${props.type === "Large" && LargeButtonStyle}
   ${props.type === "Long" && LongButtonStyle}
   ${props.type === "LargeDropDown" && LargeDropDownStyle}
   ${props.type === "SmallDropDown" && SmallDropDownStyle}
+
+  &:hover {
+    background-color: ${props.color};
+    color: #ffffff;
+  }
 `;
 
 export const Button = styled.div`
   width: 125px;
   height: 50px;
-  border: 2px solid #ffcfcf;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   padding: 10px;
   display: flex;
@@ -61,6 +75,5 @@ export const Button = styled.div`
 `;
 
 Button.defaultProps = {
-  // eslint-disable-next-line comma-dangle
-  color: "basic",
+  color: "#ffcfcf",
 };
