666f786c83e9ef43678fbc90f1bfef995719d729
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2021-11-02 00:54:51 +0900

    feat : 메인 페이지 이미지 호버 - 왼쪽 구현 #11
    이미지 호버 이벤트 바디 왼쪽 구현
    1:1 소개팅 , 2:2 미팅 ~ 팀 설정 버튼 추가
    호버시 약간의 부자연 스러움 존재

--- client/src/Organism/MainBodyLeft.tsx
+++ client/src/Organism/MainBodyLeft.tsx
@@ -2,11 +2,12 @@
 import React from "react";
 import meetingImage from "../assets/meetingImage.png";
 import { ImageDiv } from "../Atom/ImageDiv";
+import MainBodyLeftButtonContainer from "../Molecules/MainDivContainer";
 
 function MainBodyLeft() {
   return (
     <ImageDiv type="big" image={meetingImage}>
-      aa
+      <MainBodyLeftButtonContainer />
     </ImageDiv>
   );
 }
