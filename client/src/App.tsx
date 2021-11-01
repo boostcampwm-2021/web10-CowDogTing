c23a5c942f8dd6a316f79405e166098a96319333
jin-Pro <dnjun2@ajou.ac.kr>
2021-11-02 00:28:21 +0900

    Feat : App Router LoginPage, RegisterPage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { Global } from "@emotion/react";
+import { Route, Switch } from "react-router";
 import reset from "./util/reset";
 import LogInPage from "./Page/LogInPage";
+import RegisterPage from "./Page/RegisterPage";
 
 function App() {
   return (
     <>
       <Global styles={reset} />
-      <LogInPage />
+      <Switch>
+        <Route path="/login" component={LogInPage} />
+        <Route path="/register" component={RegisterPage} />
+      </Switch>
     </>
   );
 }
