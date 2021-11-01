522a4850397bebfb208c6b8879a8cb7cecc2caa8
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2021-11-01 19:21:51 +0900

    fix : jsx 주석 다시 추가
    오류 때문에 일단은 다시 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,9 @@
 import React from "react";
-import { Global } from "@emotion/react";
-import reset from "./util/reset";
-import LogInPage from "./Page/LogInPage";
 import MainPage from "./Page/MainPage";
 
 function App() {
   return (
     <>
-      <Global styles={reset} />
-      <LogInPage />
       <MainPage />
     </>
   );
--- client/src/Template/MainPageBody.tsx
+++ client/src/Template/MainPageBody.tsx
@@ -5,6 +5,9 @@ import { css } from "@emotion/react";
 const mainPageBodyStyle = css`
   width: 100vw;
   height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 function MainPageBody() {
