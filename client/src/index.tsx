ae47d715b5eb026e5f32a5badcd649af8a4bbe74
jin-Pro <dnjun2@ajou.ac.kr>
2021-11-01 15:28:20 +0900

    Feat : Recoil Install

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <RecoilRoot>
+      <Router>
+        <App />
+      </Router>
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById("root")
 );
