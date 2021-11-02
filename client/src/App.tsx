d0f3ef23d894547d60362dd21cea7601497221ab
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2021-11-02 22:13:55 +0900

    Feat : 팀 생성 컨테이너 UI 개발[#55]
    팀 생성 컨테이너 UI 개발함
    UI만 개발했기 때문에 이벤트는 존재하지 않음
    공통으로 사용할 수 있는 INPUTLABEL.TSX 구현

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,11 @@ import reset from "./util/reset";
 import LogInPage from "./Page/LogInPage";
 import RegisterPage from "./Page/RegisterPage";
 import MainPage from "./Page/MainPage";
+<<<<<<< HEAD
 import MyPage from "./Page/MyPage";
+=======
+import TeamCreatePage from "./Page/TeamCreatePage";
+>>>>>>> 1c22814... Feat : 팀 생성 컨테이너 UI 개발[#55]
 
 function App() {
   return (
@@ -15,7 +19,11 @@ function App() {
         <Route path="/" component={MainPage} exact />
         <Route path="/login" component={LogInPage} exact />
         <Route path="/register" component={RegisterPage} />
+<<<<<<< HEAD
         <Route path="/mypage" component={MyPage} />
+=======
+        <Route path="/teamCreate" component={TeamCreatePage} />
+>>>>>>> 1c22814... Feat : 팀 생성 컨테이너 UI 개발[#55]
         <Redirect path="/*" to="/" />
       </Switch>
     </>
