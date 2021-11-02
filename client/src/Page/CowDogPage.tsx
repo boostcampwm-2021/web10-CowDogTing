0b7e5a2d0b40c9012bdff6962914c1e07b7564a9
jin-Pro <dnjun2@ajou.ac.kr>
2021-11-02 16:01:17 +0900

    Feat : Page Routing

---
+++ client/src/Page/CowDogPage.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CowDogPage() {
+  return <div>12</div>;
+}
---
+++ client/src/Page/TeamPage.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TeamPage() {
+  return <div>1</div>;
+}
