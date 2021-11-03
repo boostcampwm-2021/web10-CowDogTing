d0b80376e6ca4ae307178e96b2c081a7b5c580fc
ddaynew365 <ddaynew365@gmail.com>
2021-11-04 00:49:36 +0900

    Feat: 요청 페이지 더미 데이터 연동[#81]

--- client/src/Page/RequestPage.tsx
+++ client/src/Page/RequestPage.tsx
@@ -1,17 +1,18 @@
 /** @jsxImportSource @emotion/react */
 import React, { useEffect, useState } from "react";
 import { css } from "@emotion/react";
-import { useLocation } from "react-router-dom";
 import ProfileList from "../Template/ProfileList";
 import ProfileModal from "../Template/ProfileModal";
-import { getCowDogInfo } from "../util/dummyData";
-import { ProfileType } from "../util/type";
+import { getRequestInfo } from "../util/dummyData";
+import { ProfileType, RequestsType, RequestType } from "../util/type";
 
 export default function RequestPage() {
-  const [datas, setDatas] = useState<ProfileType[] | null>(null);
+  const [datas, setDatas] = useState<RequestsType | null>(null);
   const [openModal, setOpenModal] = useState<number | null>(null);
-  const searchParams = new URLSearchParams(useLocation().search);
-  const person = Number(searchParams.get("person"));
+  const myId = "123";
+  const person = 1;
+  const [RequestForMe, setRequestForMe] = useState<ProfileType[]>([]);
+  const [RequestToMe, setRequestToMe] = useState<ProfileType[]>([]);
 
   const RequestPageStyle = css`
     display: flex;
@@ -20,9 +21,20 @@ export default function RequestPage() {
     flex-direction: row;
     justify-content: center;
   `;
+  const RequestListStyle = css`
+    display: flex;
+    flex-direction: column;
+  `;
   const getDatas = async () => {
-    const item = await getCowDogInfo(person);
+    const item = await getRequestInfo();
     setDatas(item);
+    divideRequest();
+  };
+
+  const divideRequest = () => {
+    datas?.data.forEach((data: RequestType) => {
+      return data.from === myId ? setRequestForMe((prev) => [...prev, data.info]) : setRequestToMe((prev) => [...prev, data.info]);
+    });
   };
 
   useEffect(() => {
@@ -31,14 +43,17 @@ export default function RequestPage() {
   return (
     <>
       <div css={RequestPageStyle}>
-        <ProfileList datas={datas} person={person} setOpenModal={setOpenModal} />
-
-        {datas && openModal !== null && <ProfileModal data={datas[Number(openModal)]} />}
-        <ProfileList datas={datas} person={person} setOpenModal={setOpenModal} />
-
-        {datas && openModal !== null && <ProfileModal data={datas[Number(openModal)]} />}
+        <div css={RequestListStyle}>
+          <div>나에게 온 요청</div>
+          <ProfileList datas={RequestForMe} person={person} setOpenModal={setOpenModal} />
+          {datas && openModal !== null && <ProfileModal data={RequestForMe[Number(openModal)]} />}
+        </div>
+        <div css={RequestListStyle}>
+          <div>내가 보낸 요청</div>
+          <ProfileList datas={RequestToMe} person={person} setOpenModal={setOpenModal} />
+          {datas && openModal !== null && <ProfileModal data={RequestToMe[Number(openModal)]} />}
+        </div>
       </div>
-      <div>나에게 온 요청</div>
     </>
   );
 }
