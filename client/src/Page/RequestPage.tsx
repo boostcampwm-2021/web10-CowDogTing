848c3580a9abc27d8c0791b95e09cce709999471
ddaynew365 <ddaynew365@gmail.com>
2021-11-04 01:23:13 +0900

    Fix: 데이터 비동기 처리 오류 수정[#81]

--- client/src/Page/RequestPage.tsx
+++ client/src/Page/RequestPage.tsx
@@ -4,15 +4,14 @@ import { css } from "@emotion/react";
 import ProfileList from "../Template/ProfileList";
 import ProfileModal from "../Template/ProfileModal";
 import { getRequestInfo } from "../util/dummyData";
-import { ProfileType, RequestsType, RequestType } from "../util/type";
+import { ProfileType, RequestType } from "../util/type";
 
 export default function RequestPage() {
-  const [datas, setDatas] = useState<RequestsType | null>(null);
+  const [RequestForMe, setRequestForMe] = useState<ProfileType[]>([]);
+  const [RequestToMe, setRequestToMe] = useState<ProfileType[]>([]);
   const [openModal, setOpenModal] = useState<number | null>(null);
   const myId = "123";
   const person = 1;
-  const [RequestForMe, setRequestForMe] = useState<ProfileType[]>([]);
-  const [RequestToMe, setRequestToMe] = useState<ProfileType[]>([]);
 
   const RequestPageStyle = css`
     display: flex;
@@ -25,14 +24,10 @@ export default function RequestPage() {
     display: flex;
     flex-direction: column;
   `;
+
   const getDatas = async () => {
     const item = await getRequestInfo();
-    setDatas(item);
-    divideRequest();
-  };
-
-  const divideRequest = () => {
-    datas?.data.forEach((data: RequestType) => {
+    item?.data.forEach((data: RequestType) => {
       return data.from === myId ? setRequestForMe((prev) => [...prev, data.info]) : setRequestToMe((prev) => [...prev, data.info]);
     });
   };
@@ -40,18 +35,19 @@ export default function RequestPage() {
   useEffect(() => {
     getDatas();
   }, []);
+
   return (
     <>
       <div css={RequestPageStyle}>
         <div css={RequestListStyle}>
           <div>나에게 온 요청</div>
           <ProfileList datas={RequestForMe} person={person} setOpenModal={setOpenModal} />
-          {datas && openModal !== null && <ProfileModal data={RequestForMe[Number(openModal)]} />}
+          {RequestForMe && openModal !== null && <ProfileModal data={RequestForMe[Number(openModal)]} />}
         </div>
         <div css={RequestListStyle}>
           <div>내가 보낸 요청</div>
           <ProfileList datas={RequestToMe} person={person} setOpenModal={setOpenModal} />
-          {datas && openModal !== null && <ProfileModal data={RequestToMe[Number(openModal)]} />}
+          {RequestToMe && openModal !== null && <ProfileModal data={RequestToMe[Number(openModal)]} />}
         </div>
       </div>
     </>
