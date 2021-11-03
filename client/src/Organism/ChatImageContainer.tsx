a0e60db15b099c3bf1c9e9db7aaaed2ff3e6d524
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2021-11-04 02:03:05 +0900

    Feat : mini 이미지 클릭 시 프로필 모달 띄워주기[#71]

--- client/src/Organism/ChatImageContainer.tsx
+++ client/src/Organism/ChatImageContainer.tsx
@@ -1,10 +1,12 @@
+/* eslint-disable no-console */
 /** @jsxImportSource @emotion/react */
-import React from "react";
+import React, { useState } from "react";
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 import { ChatImageContainerType } from "../util/type";
 import { ProfileImage } from "../Atom/ProfileImage";
 import { Button } from "../Atom/Button";
+import ProfileModal from "../Template/ProfileModal";
 
 const ChatListHeader = styled.div`
   display: flex;
@@ -23,15 +25,27 @@ const ChatImageContainerStyle = css`
 `;
 
 function ChatImageContainer({ member }: ChatImageContainerType) {
+  const [openModal, setOpenModal] = useState<number | null>(null);
+  const changeOpenModal = (event: React.MouseEvent) => {
+    const { id } = (event.target as HTMLElement).dataset;
+    if (id === undefined) {
+      setOpenModal(null);
+    } else if (id !== undefined) {
+      setOpenModal(Number(id));
+    }
+  };
   return (
-    <ChatListHeader>
-      <div css={ChatImageContainerStyle}>
-        {member?.map((userInfo, idx) => (
-          <ProfileImage type="Mini" image={userInfo.image} data-id={idx} />
-        ))}
-      </div>
-      <Button>나가기</Button>
-    </ChatListHeader>
+    <>
+      <ChatListHeader onClick={changeOpenModal}>
+        <div css={ChatImageContainerStyle}>
+          {member?.map((userInfo, idx) => (
+            <ProfileImage type="Mini" image={userInfo.image} data-id={idx} />
+          ))}
+        </div>
+        <Button>나가기</Button>
+      </ChatListHeader>
+      {member && openModal !== null && <ProfileModal data={member[openModal]} />}
+    </>
   );
 }
 
