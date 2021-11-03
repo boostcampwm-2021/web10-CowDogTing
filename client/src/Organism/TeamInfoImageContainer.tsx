e41acc327af888a296a041690576b932b9d54fcb
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2021-11-03 16:56:44 +0900

    fix : 이미지 업로드 버그 수정[#63]

--- client/src/Organism/TeamInfoImageContainer.tsx
+++ client/src/Organism/TeamInfoImageContainer.tsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import React, { ChangeEvent, ChangeEventHandler, useRef, useState } from "react";
+import React, { ChangeEvent, ChangeEventHandler, useEffect, useRef, useState } from "react";
 import { ProfileImage } from "../Atom/ProfileImage";
 import { TeamImageContainerType } from "../util/type";
 
@@ -17,22 +17,27 @@ const imageInputStyle = css`
 `;
 
 function TeamInfoImageContainer({ image }: TeamImageContainerType) {
+  const initBlob = new Blob();
   const imageInputTag = useRef<HTMLInputElement | null>(null);
   const [profileImage, setProfileImage] = useState<string | ArrayBuffer | null>(image);
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [imageFile, setImageFile] = useState<Blob>(new Blob());
+  const [imageFile, setImageFile] = useState<Blob>(initBlob);
   const clickImageTag = () => {
     (imageInputTag.current as HTMLInputElement).click();
   };
   const changeImage: ChangeEventHandler<HTMLInputElement> = (event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.files === null) return;
     setImageFile(event.target.files[0]);
+  };
+  useEffect(() => {
+    if (imageFile === initBlob) return;
     const reader = new FileReader();
     reader.onloadend = () => {
       setProfileImage(reader.result);
     };
-    reader.readAsDataURL(event.target.files[0]);
-  };
+    reader.readAsDataURL(imageFile);
+  }, [imageFile]);
+
   return (
     <div css={TeamInfoImageContainerStyle}>
       <ProfileImage type="Big" image={profileImage} onClick={clickImageTag} />
