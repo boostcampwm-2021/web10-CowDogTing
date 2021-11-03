38da5aa00c88806402529562589bdb63e985bb09
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2021-11-04 01:13:06 +0900

    Feat : 채팅 목록 페이지 - 채팅방 전체 UI 구현[#66]

--- client/src/Organism/ChatListContainer.tsx
+++ client/src/Organism/ChatListContainer.tsx
@@ -1,8 +1,13 @@
 /** @jsxImportSource @emotion/react */
 import React from "react";
 import { css } from "@emotion/react";
+import { Link } from "react-router-dom";
 import { ChatListContainerType } from "../util/type";
 import ChatImageContainer from "./ChatImageContainer";
+import ChatDetail from "./ChatDetail";
+import ChatInput from "../Molecules/ChatInput";
+import ChatInputContainer from "./ChatInputContainer";
+import { Button } from "../Atom/Button";
 
 const ChatListContainerStyle = css`
   width: 50%;
@@ -13,9 +18,21 @@ const ChatListContainerStyle = css`
 `;
 
 function ChatListContainer({ chatInfo }: ChatListContainerType) {
+  const chatRoomId = chatInfo?.chatRoomID;
   return (
     <div css={ChatListContainerStyle}>
       <ChatImageContainer member={chatInfo?.member} />
+      {chatRoomId !== undefined && (
+        <>
+          <ChatDetail chatRoomID={chatRoomId} />
+          <ChatInputContainer>
+            <ChatInput />
+            <Link to={`/sub/ChatRoom?chatRoomId=${chatRoomId}`}>
+              <Button>추가기능</Button>
+            </Link>
+          </ChatInputContainer>
+        </>
+      )}
     </div>
   );
 }
