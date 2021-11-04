ace2f495d1be3596506b1afcd20badba6bc3abc5
ddaynew365 <ddaynew365@gmail.com>
2021-11-04 14:59:57 +0900

    Feat: server sequlize association 수정[#87]

--- server/src/models/chatRoom.ts
+++ server/src/models/chatRoom.ts
@@ -1,33 +1,38 @@
 import { DataTypes, Model } from "sequelize";
-import { Chat } from "./chat";
 import { EnterRoom } from "./enterRoom";
 import { sequelize } from "./index";
+import { Users } from "./users";
 
 interface ChatRoomAttributes {
-    chatRoomId : number;
+  chatRoomId: number;
 }
 
-
-export class ChatRoom extends Model<ChatRoomAttributes>{
-    public chatRoomId !: number;
-    public static associations: {};
+export class ChatRoom extends Model<ChatRoomAttributes> {
+  public chatRoomId!: number;
+  public static associations: {};
 }
 
 ChatRoom.init(
-    {
-        chatRoomId : {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement:true
-        }
-    
+  {
+    chatRoomId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        modelName : 'ChatRoom',
-        tableName : 'ChatRoom',
-        sequelize,
-        freezeTableName : true,
-    }
+  },
+  {
+    modelName: "ChatRoom",
+    tableName: "ChatRoom",
+    sequelize,
+    freezeTableName: true,
+    timestamps: false,
+  }
 );
 
+Users.belongsToMany(ChatRoom, {
+  through: EnterRoom,
+});
+ChatRoom.belongsToMany(Users, {
+  through: EnterRoom,
+});
--- server/src/models/request.ts
+++ server/src/models/request.ts
@@ -4,54 +4,48 @@ import { SendRequest } from "./sendRequest";
 import { Users } from "./users";
 
 interface RequestAttributes {
-    requestId: number;
-    to: string;
-    state: string;
-    uid: string;
+  requestId: number;
+  to: string;
+  state: string;
 }
 
+export class Request extends Model<RequestAttributes> {
+  public requestId!: number;
+  public to!: string;
+  public state!: string;
 
-export class Request extends Model<RequestAttributes>{
-    public requestId!: number;
-    public to!: string;
-    public state!: string;
-    public uid!: string;
-
-    public static associations: {};
+  public static associations: {};
 }
 
 Request.init(
-    {
-        requestId: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-        },
-        to: {
-            type: DataTypes.STRING(30),
-            allowNull: false
-        },
-        state: {
-            type: DataTypes.STRING(10),
-            allowNull: false
-        },
-        uid: {
-            type: DataTypes.STRING(30),
-            allowNull: false
-        }
-    
+  {
+    requestId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
     },
-    {
-        modelName : 'Request',
-        tableName : 'Request',
-        sequelize,
-        freezeTableName : true,
-    }
+    to: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+    },
+    state: {
+      type: DataTypes.STRING(10),
+      allowNull: false,
+    },
+  },
+  {
+    modelName: "Request",
+    tableName: "Request",
+    sequelize,
+    freezeTableName: true,
+  }
 );
 
 Users.hasMany(Request, {
-    foreignKey: 'uid',
-})
+  foreignKey: "from",
+  sourceKey: "uid",
+});
 Request.belongsTo(Users, {
-    foreignKey: 'uid',
-})
+  foreignKey: "from",
+  targetKey: "uid",
+});
