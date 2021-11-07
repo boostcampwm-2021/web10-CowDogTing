4d857bbfe88b79fba194624352e7b70ac431792b
jin-Pro <dnjun2@ajou.ac.kr>
2021-11-07 17:34:42 +0900

    Refactor : MyPage Create ImageContainer

--- client/src/Template/MyPageMain/MyInfo.tsx
+++ client/src/Template/MyPageMain/MyInfo.tsx
@@ -2,15 +2,20 @@
 
 import { css } from "@emotion/react";
 import MyInfoContainer from "../../Organism/MyInfoContainer";
+import TeamInfoImageContainer from "../../Organism/TeamInfoImageContainer";
+import teamImage from "../../assets/meetingImage.png";
 
 const MyInfoStyle = css`
   padding-left: 50px;
   width: 60vw;
+  display: flex;
+  align-items: center;
 `;
 export default function MyInfo() {
   return (
     <div css={MyInfoStyle}>
       <MyInfoContainer />
+      <TeamInfoImageContainer image={teamImage} />
     </div>
   );
 }
