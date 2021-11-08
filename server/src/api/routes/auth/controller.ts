79b00b5c9db85126d369e0a4feeff8141d21bec6
ddaynew365 <ddaynew365@gmail.com>
2021-11-08 21:53:35 +0900

    Fix: 로그인 쿠키 안받는 문제 해결

--- server/src/api/routes/auth/controller.ts
+++ server/src/api/routes/auth/controller.ts
@@ -1,7 +1,5 @@
 import * as express from "express";
 import * as passport from "passport";
-import * as bcrypt from "bcrypt";
-import { Users } from "../../../models/users";
 import { findUser, createUser } from "./service";
 
 export const handleJoin = async (req, res, next) => {
@@ -11,7 +9,6 @@ export const handleJoin = async (req, res, next) => {
     if (exUser) {
       return res.send({ test: "해당 아이디 존재" });
     }
-    const hash: string = await bcrypt.hash(password, 12); // bcrypt의 첫 번째 인자는 암호화 대상, 두번 째 인자는 pbkdf2의 반복 횟수(숫자가 커질수록 비밀번호를 알아내기 어렵지만 암호화 시간도 오래걸린다. 보통 12이상을 추천)
     await createUser({ uid, password, location, image, age, sex });
     return res.send({ test: "회원가입 성공" });
   } catch (error) {
@@ -34,13 +31,9 @@ export const handleLogin = (req, res, next) => {
         console.error(loginError);
         return next(loginError);
       }
-      res.cookie("sibal", "sibalsaeki");
       return res.send({ test: "로그인 성공" });
-      // return res.redirect("http://localhost:3001/main");
     });
   })(req, res, next);
-  // res.cookie("rememberme", JSON.stringify("1"), { expires: new Date(Date.now() + 900000), httpOnly: false });
-  res.send({ test: "로그인 성공" });
 };
 
 export const handleLogOut = (req, res) => {
