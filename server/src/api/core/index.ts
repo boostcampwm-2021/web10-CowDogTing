97081ff29ed78a29496dec246187d2b844aac1db
jin-Pro <dnjun2@ajou.ac.kr>
2021-11-08 23:38:59 +0900

    Feat : Core API [#92, #91]

--- server/src/api/core/index.ts
+++ server/src/api/core/index.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { getImage, getJoinChatInfo } from "./controller";
+import { getImage, getUserInfo, getJoinChatInfo } from "./controller";
 const coreRouter = Router();
 
 coreRouter.get("/image", getImage);
-coreRouter.get("/userInfo");
+coreRouter.get("/userInfo", getUserInfo);
 coreRouter.get("/joinChatInfo", getJoinChatInfo);
 
 export default coreRouter;
--- server/src/api/core/service.ts
+++ server/src/api/core/service.ts
@@ -1,9 +1,119 @@
-export const findImage = async ({ imageID }) => {
-  // imageID 에 대한 db 값 가져오기
+import { Chat } from "../../models/chat";
+import { ChatRoom } from "../../models/chatRoom";
+import { Users } from "../../models/users";
+
+/**
+ *
+ * @param param0 => {imageId}
+ * @returns  =>{
+                  "image": "string"
+                }
+ *
+ * /core/controller/getImage
+ */
+export const findImage = async ({ imageId }) => {
+  const query = {
+    raw: true,
+    attributes: ["image"],
+    where: { id: imageId },
+  };
+
+  const data = await Image.findOne(query);
+  return data;
 };
+
+/**
+ *
+ * @param param0 => { uid }
+ * @returns =>{
+                "id": "yj",
+                "image": 33,
+                "location": "우만동",
+                "sex": "male",
+                "age": 25,
+                "info": "안녕하세요"
+              }
+ *
+ * /core/controller/getUserInfo
+ */
+export const findUserInfo = async ({ uid }) => {
+  const query = {
+    raw: true,
+    attributes: ["uid", "image", "location", "sex", "age", "info"],
+    where: { uid },
+    include: {
+      model: Image,
+      where: { id: image },
+      attributes: ["image"],
+    },
+  };
+
+  const data = await Users.findOne(query);
+  return data;
+};
+
+/**
+ *
+ * @param param0 => { uid }
+ * @returns =>[
+                {
+                  "chatRoomID": 1,
+                  "notReadNum": 3
+                },
+                {
+                  "chatRoomID": 2,
+                  "notReadNum": 2
+                }
+              ]
+ * /core/controller/getJoinChatInfo
+ */
 export const findChatRoomNotReadNum = async ({ uid }) => {
-  // uid로 chatRoom의 id값 가져와서
-  // join으로 message 가져오는데
-  // read : false 인 애들 만 가져와서
-  // 숫자로 변환해서 return 해주기
+  const query = {
+    raw: true,
+    attributes: ["chatRoomId"],
+    where: { uid },
+    include: [
+      {
+        raw: true,
+        model: Chat,
+        where: { chatRoomId, Read: false },
+        attributes: ["chatId"],
+      },
+    ],
+  };
+
+  // datas = [
+  //   {
+  //     chatRoomId: 0,
+  //     Chat: [
+  //       {
+  //         chatId: 1,
+  //       },
+  //       {
+  //         chatId: 2,
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     chatRoomId: 0,
+  //     Chat: [
+  //       {
+  //         chatId: 1,
+  //       },
+  //       {
+  //         chatId: 2,
+  //       },
+  //     ],
+  //   },
+  // ];
+  const datas = await ChatRoom.findAll(query);
+  const data = datas.map((item) => {
+    const { chatRoomId, Chat } = item;
+    return {
+      chatRoomId,
+      notReadNum: Chat.length,
+    };
+  });
+
+  return data;
 };
