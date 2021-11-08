f5f26971b51a99f9bdd0e4e7773e47e5780632a9
jin-Pro <dnjun2@ajou.ac.kr>
2021-11-09 00:31:01 +0900

    Feat : Team API get TeamInfo [#94]

--- server/src/api/team/controller.ts
+++ server/src/api/team/controller.ts
@@ -1,20 +1,35 @@
 import { _createTeam, findTeam, _updateTeam, _inviteTeam } from "./service";
 
-export const getTeamInfo = async (res, req) => {
-  const gid = res.teamID;
-  const data = await findTeam({ gid });
+/**
+ *
+ * @param res
+ * @param req
+ *
+ * [get] : /api/team/info
+ */
+export const getTeamInfo = async (req, res) => {
+  const {
+    query: { teamId: gid },
+  } = req;
+
+  try {
+    const data = await findTeam({ gid });
+    res.send(data);
+  } catch (err) {
+    res.send(err);
+  }
 };
-export const createTeam = async (res, req) => {
-  const teamInfo = res.teamInfo;
+export const createTeam = async (req, res) => {
+  const { teamInfo } = res;
   await _createTeam({ teamInfo });
 };
-export const updateTeam = async (res, req) => {
-  const teamInfo = res.teamInfo;
+export const updateTeam = async (req, res) => {
+  const { teamInfo } = res;
   await _updateTeam({ teamInfo });
 };
-export const inviteTeam = async (res, req) => {
+export const inviteTeam = async (req, res) => {
   // swagger에 잘못 나와있는거가틈. 팀 아이디가 없음
   const gid = res.teamID;
-  const inviteID = res.inviteID;
+  const { inviteID } = res;
   await _inviteTeam({ gid, inviteID });
 };
--- server/src/api/team/index.ts
+++ server/src/api/team/index.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { createTeam, getTeamInfo, inviteTeam, updateTeam } from "./controller";
-const teamRouter = Router();
 
+const teamRouter = Router();
 teamRouter.get("/info", getTeamInfo);
 teamRouter.post("/invite", inviteTeam);
 teamRouter.post("/update", updateTeam);
