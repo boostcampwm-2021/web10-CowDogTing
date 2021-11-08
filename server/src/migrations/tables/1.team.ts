37df0d607625912942326330fd031fc6e9938013
ddaynew365 <ddaynew365@gmail.com>
2021-11-08 21:53:35 +0900

    Fix: 로그인 쿠키 안받는 문제 해결

--- server/src/migrations/tables/1.team.ts
+++ server/src/migrations/tables/1.team.ts
@@ -1,13 +1,13 @@
-import { Team } from '../../models/team';
+import { Team } from "../../models/team";
 
-console.log('======Create users Table======');
+console.log("======Create users Table======");
 const create_table_groups = async () => {
-  await Team.sync({ force: true })
+  await Team.sync({ force: true }) // 데이터 생성 이후 false로
     .then(() => {
-      console.log('✅Success Create users Table');
+      console.log("✅Success Create users Table");
     })
     .catch((err) => {
-      console.log('❗️Error in Create users Table : ', err);
+      console.log("❗️Error in Create users Table : ", err);
     });
 };
 
