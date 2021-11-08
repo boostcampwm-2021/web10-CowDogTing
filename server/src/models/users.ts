7fe3a0b28ae32f9ecf5e9102d763330426916db8
jin-Pro <dnjun2@ajou.ac.kr>
2021-11-08 23:12:44 +0900

    Fix : Prettier

--- server/src/models/users.ts
+++ server/src/models/users.ts
@@ -16,13 +16,21 @@ interface UserAttributes {
 
 export class Users extends Model<UserAttributes> {
   public uid!: string;
+
   public password!: string;
+
   public location!: string;
+
   public github_id!: string;
+
   public naver_id!: string;
+
   public kakao_id!: string;
+
   public image!: string;
+
   public age!: number;
+
   public sex!: string;
 
   public static associations: {};
