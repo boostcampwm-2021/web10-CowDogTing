29e383ae55a86631b5a503a8e847ced6207bda72
ddaynew365 <ddaynew365@gmail.com>
2021-11-09 14:00:20 +0900

    Refactor: migration 파일 리팩토링[#117]

--- server/src/migrations/tables/5.participant.ts
+++ server/src/migrations/tables/5.participant.ts
@@ -1,13 +1,13 @@
 import { Participant } from "../../models/participant";
 
-console.log("======Create users Table======");
+console.log("======Create participant Table======");
 const create_table_participant = async () => {
   await Participant.sync({ force: true })
     .then(() => {
-      console.log("✅Success Create users Table");
+      console.log("✅Success Create participant Table");
     })
     .catch((err) => {
-      console.log("❗️Error in Create users Table : ", err);
+      console.log("❗️Error in Create participant Table : ", err);
     });
 };
 
