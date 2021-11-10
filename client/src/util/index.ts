551b8e4c3f1b9a58dd22a2d05858f0cdfdac849c
taehong0-0 <xoghd5885@ajou.ac.kr>
2021-11-10 22:01:28 +0900

    Feat : Create Team [#111]

--- client/src/util/index.ts
+++ client/src/util/index.ts
@@ -1,10 +1,21 @@
+import axios from "axios";
+
 const url = `${process.env.REACT_APP_SERVER_URL}:${process.env.REACT_APP_SERVER_PORT}`;
 
-export const changeTeamInfo = async ({ teamName, teamInfoInput, location, leader }) => {
+export const changeTeamInfo = async ({ beforeTeamName, teamName, teamInfoInput, location, leader }) => {
   await axios.post(`${url}/api/team/update`, {
-    originTeamName: "ajouUniv",
+    originTeamName: beforeTeamName,
     name: teamName,
     description: teamInfoInput,
+    location,
+    leader,
+  });
+};
+
+export const createTeam = async ({ teamName, teamInfo, location, leader }) => {
+  await axios.post(`${url}/api/team/create`, {
+    name: teamName,
+    description: teamInfo,
     location,
     leader,
   });
