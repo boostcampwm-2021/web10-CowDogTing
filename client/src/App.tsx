a553c8554236a5837c055aa9177e145b506586a6
taehong0-0 <xoghd5885@ajou.ac.kr>
2021-11-10 22:11:21 +0900

    Feat : User Info Data get

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,25 @@
-import React from "react";
+/* eslint-disable no-console */
+import React, { useEffect } from "react";
 import { Global } from "@emotion/react";
 import { Redirect, Route, Switch } from "react-router";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import reset from "./util/reset";
 import MainPage from "./Page/MainPage";
 import Page from "./Page/Page";
 import Footer from "./Molecules/Footer";
 import ChatRoom from "./Page/ChatRoom";
+import { fetchGet } from "./Recoil/Selector";
+import { userState } from "./Recoil/Atom";
 
 function App() {
+  const url = `${process.env.REACT_APP_GET_USER_INFO_API_URL}`;
+  const userInfo = useRecoilValue(fetchGet({ url, query: "?uid=test0" }));
+  const setUserInfo = useSetRecoilState(userState);
+
+  useEffect(() => {
+    setUserInfo(userInfo);
+  }, [userInfo]);
+
   return (
     <>
       <Global styles={reset} />
