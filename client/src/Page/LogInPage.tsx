aaa475cb58b749fc7da2653c2e15d3400b80a924
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2021-11-10 16:00:06 +0900

    Feat : 로그인 api 연동 확인 [close #108 ]

--- client/src/Page/LogInPage.tsx
+++ client/src/Page/LogInPage.tsx
@@ -44,8 +44,9 @@ export default function LogInPage() {
     if (!idRef.current || !pwRef.current) return;
     const id = idRef.current.value;
     const pw = pwRef.current.value;
-    const url = `${process.env.REACT_APP_SEVER_URL} + ${process.env.REACT_APP_SERVER_PORT} + ${process.env.REACT_APP_LOGIN_API_URL}`;
-    await postApi(url, { uid: id, password: pw });
+    const url = `${process.env.REACT_APP_SERVER_URL}:${process.env.REACT_APP_SERVER_PORT}${process.env.REACT_APP_LOGIN_API_URL}`;
+    // eslint-disable-next-line no-console
+    postApi(url, { uid: id, password: pw });
   };
 
   return (
