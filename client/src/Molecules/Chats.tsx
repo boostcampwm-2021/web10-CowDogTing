963bfa8a1b78c79746baac463b76f17492bac30f
jin-Pro <dnjun2@ajou.ac.kr>
2021-11-14 17:43:58 +0900

    Refactor : Chat Children

--- client/src/Molecules/Chats.tsx
+++ client/src/Molecules/Chats.tsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { useRecoilValue } from "recoil";
 import Chat from "../Atom/Chat";
-import { chatTarget, userState } from "../Recoil/Atom";
+import { chatTarget } from "../Recoil/Atom";
 
 export default function Chats() {
-  const { id: myId } = useRecoilValue(userState);
   const { chatMessage: chats } = useRecoilValue(chatTarget);
 
   return (
     <>
       {chats?.map((chat) => {
-        const type = chat.from === myId ? "Mine" : "Other";
-        return <Chat type={type}>{[chat.from, chat.message]}</Chat>;
+        const { from, message } = chat;
+        return <Chat from={from} message={message} />;
       })}
     </>
   );
