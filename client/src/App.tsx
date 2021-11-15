3b9dedc36274a5c82e343b569e2db76c3dd8732d
jin-Pro <dnjun2@ajou.ac.kr>
2021-11-16 03:13:56 +0900

    Fix : App.tsx => Socket Event Handler

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,51 +1,73 @@
+/* eslint-disable consistent-return */
 /* eslint-disable spaced-comment */
 /* eslint-disable no-console */
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Global } from "@emotion/react";
 import { Redirect, Route, Switch } from "react-router";
-import { useRecoilValue, useSetRecoilState } from "recoil";
+import { useRecoilState } from "recoil";
+// import { useRecoilState, useRecoilValue } from "recoil";
 import reset from "./util/reset";
 import MainPage from "./Page/MainPage";
 import Page from "./Page/Page";
 import Footer from "./Molecules/Footer";
 import ChatRoom from "./Page/ChatRoom";
-import { fetchGet } from "./Recoil/Selector";
-import { joinChatRoomState, requestState, userState } from "./Recoil/Atom";
+import { chatsState, joinChatRoomState, requestState, userState } from "./Recoil/Atom";
 import ErrorModal from "./Template/ErrorModal";
 import ClientSocket from "./Socket";
+import { getFetch } from "./util/data";
+import { ChatInfoType, RequestType } from "./util/type";
+import { handleReceiveAcceptSocket, handleReceiveChatSocket, handleReceiveDenySocket, handleReceiveRequestSocket } from "./util";
+import { JOIN_CHAT_URL, REQUEST_URL, USER_URL } from "./util/URL";
 
 function App() {
-  const userUrl = `${process.env.REACT_APP_GET_USER_INFO_API_URL}`;
-  const requestUrl = `${process.env.REACT_APP_GET_REQUEST_API_URL}`;
-  const joinChatUrl = `${process.env.REACT_APP_GET_JOIN_CHAT_INFO_API_URL}`;
-  const userInfo = useRecoilValue(fetchGet({ url: userUrl, query: "" }));
-  const requestInfo = useRecoilValue(fetchGet({ url: requestUrl, query: "" }));
-  const joinChatInfo = useRecoilValue(fetchGet({ url: joinChatUrl, query: "" }));
+  const [user, setUser] = useRecoilState(userState);
+  const [request, setRequest] = useRecoilState(requestState);
+  const [joinChat, setJoinChat] = useRecoilState(joinChatRoomState);
+  const [chat, setChat] = useRecoilState(chatsState);
 
-  const setUserInfo = useSetRecoilState(userState);
-  const setRequestInfo = useSetRecoilState(requestState);
-  const setJoinChatInfo = useSetRecoilState(joinChatRoomState);
+  const [socket, setSocket] = useState<any>(null);
 
   const getInitData = async () => {
     try {
-      setUserInfo(userInfo);
-      setRequestInfo(requestInfo);
-      setJoinChatInfo(joinChatInfo);
+      const userData = await getFetch({ url: USER_URL, query: "" });
+      const requestData = await getFetch({ url: REQUEST_URL, query: "" });
+      const joinChatData = await getFetch({ url: JOIN_CHAT_URL, query: "" });
+      setUser(userData);
+      setRequest(requestData);
+      setJoinChat(joinChatData);
     } catch (error) {
       console.log(error);
     }
   };
 
   useEffect(() => {
-    if (userInfo.id === "") return;
-    // eslint-disable-next-line no-new
-    new ClientSocket(userInfo.id);
-  }, [userInfo]);
+    if (socket === null) return;
+
+    const handleReceiveRequestEvent = (data: RequestType) => {
+      handleReceiveRequestSocket({ setRequest, data });
+    };
+    const handleReceiveDenyEvent = (data: { from: string; to: string }) => {
+      handleReceiveDenySocket({ setRequest, data });
+    };
+    const handleReceiveAcceptEvent = (data: { chat: ChatInfoType; from: string; to: string }) => {
+      handleReceiveAcceptSocket({ setRequest, setJoinChat, setChat, data });
+    };
+    const handleReceiveChatEvent = (data: ChatInfoType) => {
+      handleReceiveChatSocket({ setJoinChat, setChat, data });
+    };
+
+    socket.addEvent({ handleReceiveRequestEvent, handleReceiveDenyEvent, handleReceiveAcceptEvent, handleReceiveChatEvent });
+    return () => {
+      socket.deleteEvent({ handleReceiveRequestEvent, handleReceiveDenyEvent, handleReceiveAcceptEvent, handleReceiveChatEvent });
+    };
+  }, [request, joinChat, chat]);
 
   useEffect(() => {
-    if (userInfo.id === "") return;
+    if (user.id === "") return;
+    // eslint-disable-next-line no-new
+    setSocket(new ClientSocket(user.id));
     getInitData();
-  }, [userInfo, requestInfo, joinChatInfo]);
+  }, [user]);
 
   return (
     <>
