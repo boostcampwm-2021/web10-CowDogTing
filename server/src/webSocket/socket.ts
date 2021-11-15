009f751763d51e53d5672e05516d01d5432b2eae
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2021-11-15 16:24:34 +0900

    Feat : 서버 소켓 연결

---
+++ server/src/webSocket/socket.ts
@@ -0,0 +1,19 @@
+import express = require("express");
+import { Server } from "socket.io";
+
+export const socketInit = (server: any, app: express.Application) => {
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
+
+  app.set("io", io);
+  io.on("connection", (socket) => {
+    const req = socket.request;
+    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    console.log("socket 연결 성공 ip : ", ip);
+    console.log(socket.id);
+  });
+};
