128cd7d44d00a964ea6769253840d111c567db9d
taehong0-0 <xoghd5885@ajou.ac.kr>
2021-11-16 02:36:32 +0900

    Fix : Merge Conflict

--- server/src/passport/index.ts
+++ server/src/passport/index.ts
@@ -1,11 +1,8 @@
 import * as passport from "passport";
 import local from "./localStrategy";
-<<<<<<< HEAD
 import naver from "./naverStrategy";
-=======
 import kakao from "./kakaoStrategy";
 import github from "./githubStrategy";
->>>>>>> ef0a78fd4f8516be0f386d8d06a120e36d273ed1
 import { Users } from "../db/models/users";
 
 export default () => {
@@ -19,10 +16,7 @@ export default () => {
       .catch((err) => done(err));
   });
   local();
-<<<<<<< HEAD
   naver();
-=======
   kakao();
   github();
->>>>>>> ef0a78fd4f8516be0f386d8d06a120e36d273ed1
 };
