36d6d9b1742cb41d6d254b84e05c42b112f0b84f
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2021-11-15 17:15:46 +0900

    Feat : 서버  setUid 이벤트 추가

--- server/src/webSocket/socket.ts
+++ server/src/webSocket/socket.ts
@@ -1,6 +1,7 @@
 import express = require("express");
 import { Server } from "socket.io";
 
+const SocketMap = new Map<string, string>();
 export const socketInit = (server: any, app: express.Application) => {
   const io = new Server(server, {
     cors: {
@@ -15,5 +16,8 @@ export const socketInit = (server: any, app: express.Application) => {
     const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
     console.log("socket 연결 성공 ip : ", ip);
     console.log(socket.id);
+    socket.on("setUid", (id: string) => {
+      SocketMap.set(id, socket.id);
+    });
   });
 };
