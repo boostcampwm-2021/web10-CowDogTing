497f5798243fef2c0011abd6194de604eefce72e
taehong0-0 <xoghd5885@ajou.ac.kr>
2021-11-16 18:03:29 +0900

    Refactor : LoginPage 리팩토링 회원가입, 로그인 버튼 분리 소셜로그인 버튼 컨테이너 분리

---
+++ client/src/Molecules/LoginButtonContainer.tsx
@@ -0,0 +1,21 @@
+import { css } from "@emotion/react";
+import { Link } from "react-router-dom";
+import { Button } from "../Atom/Button";
+
+const ButtonContainerStyle = css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+export default function LoginButtonContainer(onClick: any) {
+  return (
+    <div css={ButtonContainerStyle}>
+      <Link to="/sub/Register">
+        <Button type="Small">회원가입</Button>
+      </Link>
+      <Button type="Small" onClick={onClick}>
+        로그인
+      </Button>
+    </div>
+  );
+}
---
+++ client/src/Molecules/SocialLoginContainer.tsx
@@ -0,0 +1,32 @@
+import { css } from "@emotion/react";
+import { Button } from "../Atom/Button";
+
+const SocialLoginButtonContainerStyle = css`
+  div:nth-child(1) {
+    margin-top: 20px;
+  }
+  div:nth-child(2) {
+    margin: 10px 0;
+  }
+`;
+export default function SocialLoginContainer() {
+  return (
+    <div css={SocialLoginButtonContainerStyle}>
+      <a href="http://localhost:4000/api/auth/naver">
+        <Button type="Long" color="#2DB400">
+          Sign in with Naver
+        </Button>
+      </a>
+      <a href="http://localhost:4000/api/auth/github">
+        <Button type="Long" color="#000000">
+          Sign in with Github
+        </Button>
+      </a>
+      <a href="http://localhost:4000/api/auth/kakao">
+        <Button type="Long" color="#f3e84d">
+          Sign in with Kakao
+        </Button>
+      </a>
+    </div>
+  );
+}
