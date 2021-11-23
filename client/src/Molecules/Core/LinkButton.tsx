7fb3c81e379b97a3039342c24de0c4c72d93d5f3
ddaynew365 <ddaynew365@gmail.com>
2021-11-23 21:51:58 +0900

    Feat : Not Readt Message Num Counting Component [#242]

--- client/src/Molecules/Core/LinkButton.tsx
+++ client/src/Molecules/Core/LinkButton.tsx
@@ -4,13 +4,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Button } from "../../Atom/Button";
 import { LinkButtonType } from "../../util/type";
+import NotReadNum from "../Chat/NotReadNum";
 
 function LinkButton({ url, type, content, onClick, refProps, id }: LinkButtonType) {
+  console.log(content);
   return url && url !== "" ? (
     <Link to={url}>
       <Button type={type} onClick={onClick} ref={refProps}>
         {content}
       </Button>
+      {checkChatElement(content) && <NotReadNum />}
     </Link>
   ) : (
     <Button type={type} onClick={onClick} ref={refProps} className={content === "로그아웃" ? "logout" : ""} data-id={id}>
@@ -19,3 +22,7 @@ function LinkButton({ url, type, content, onClick, refProps, id }: LinkButtonTyp
   );
 }
 export default LinkButton;
+
+function checkChatElement(type: string) {
+  return type === "채팅 요청 목록";
+}
