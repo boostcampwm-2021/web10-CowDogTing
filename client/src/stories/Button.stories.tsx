f0f5f659373699e5639468086357104d69a217d3
taehong0-0 <xoghd5885@ajou.ac.kr>
2021-11-30 16:53:05 +0900

    Feat : Add storybook

---
+++ client/src/stories/Button.stories.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Button } from "../Atom/Button";
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Example/Button",
+  component: Button,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+} as ComponentMeta<typeof Button>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const normal = Template.bind({});
+normal.args = {};
---
+++ client/src/stories/DropDown.stories.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import DropDown from "../Molecules/Core/DropDown";
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Example/DropDown",
+  component: DropDown,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+} as ComponentMeta<typeof DropDown>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof DropDown> = (args) => <DropDown {...args} />;
+
+export const location = Template.bind({});
+location.args = { type: "Location" };
+export const age = Template.bind({});
+age.args = { type: "Age" };
+export const sex = Template.bind({});
+sex.args = { type: "Sex" };
+export const chat = Template.bind({});
+chat.args = { type: "Chat" };
+export const menu = Template.bind({});
+menu.args = { type: "Menu" };
+export const meeting = Template.bind({});
+meeting.args = { type: "Meeting" };
+export const user = Template.bind({});
+user.args = { type: "User" };
