77b885da95529785e35d90d5970f51aecb5db825
jin-Pro <dnjun2@ajou.ac.kr>
2022-08-08 22:27:56 +0900

    enhance: RegisterPage.Style.tsx => style 이관

---
+++ client/src/Component/Page/RegisterPage/RegisterPage.style.tsx
@@ -0,0 +1,38 @@
+import { css } from "@emotion/react";
+
+export const RegisterContainerStyle = css`
+  width: 450px;
+  margin: 0 auto;
+  margin-top: 50px;
+  select {
+    height: 50px;
+  }
+`;
+
+export const IdContainerStyle = css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const checkPasswordStyle = css`
+  text-align: end;
+  color: red;
+`;
+
+export const passwordCheckContainerStyle = css`
+  width: 300px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const InfoStyle = css`
+  width: 300px;
+  height: 50px;
+  align-items: center;
+  display: flex;
+  justify-content: space-around;
+  border: 2px solid #ffcfcf;
+  margin-bottom: 20px;
+  text-align: center;
+`;
